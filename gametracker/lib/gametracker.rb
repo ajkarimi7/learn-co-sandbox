3578fb126bb029e34b143551eaf9961620823f21
Ali Karimi <ajkarimi7@gmail.com>
2019-10-11 09:06:12 +0000

    Automatically backed up by Learn

---
+++ gametracker/lib/gametracker.rb
@@ -0,0 +1,6 @@
+require "gametracker/version"
+
+module Gametracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ gametracker/lib/gametracker/version.rb
@@ -0,0 +1,3 @@
+module Gametracker
+  VERSION = "0.1.0"
+end
